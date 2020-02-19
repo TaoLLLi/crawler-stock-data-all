030d45ef1ca78b358132c5c2f5163aa76958c35f
ChinaShuai <11193866572@qq.com>
2020-02-19 13:05:21 +0800

    add sql

--- crawler-stock-data-all/crawler-ths-data-crawl/src/main/java/com/ths/ScheduleApplication.java
+++ crawler-stock-data-all/crawler-ths-data-crawl/src/main/java/com/ths/ScheduleApplication.java
@@ -8,6 +8,15 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class ScheduleApplication {
 
+    /********************************************************************************
+     * 运行前先创建好数据表，语句在：
+     *
+     * sql/东方财富-资金流向-建表语句/stock_dfcf_fund_flow_info.sql
+     * sql/同花顺-板块信息-建表语句/stock_ths_gn_info.sql
+     *
+     * ********************************************************************************
+     * @param args
+     */
     public static void main(String[] args) {
         SpringApplication.run(ScheduleApplication.class, args);
     }
