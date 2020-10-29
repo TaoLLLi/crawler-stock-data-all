0280e9620b73f2c4fd4acae17969e848ab14c097
Administrator <Administrator@DESKTOP-4RPB9R2>
2020-10-29 08:52:25 +0800

    删除

--- crawler-stock-data-all/crawler-ths-data-crawl/src/main/java/com/ths/constant/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.ths.constant;
-
-public class Constants {
-
-	public static final String CHROMEDRIVER_STRING = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe";
-}
